bf8a1be1aedfb01c6c04acac0de0dbf033a92ea1
unknown <gamberfox@gmail.com>
2023-02-16 19:27:58 -0500

    starting

--- python/scrape.py
+++ python/scrape.py
@@ -4,5 +4,5 @@ import requests
 from bs4 import BeautifulSoup #web scraping
 import smtplib #send email
 from email.mime.multipart import MIMEMultipart #email body
-from email.mime.text import MIMEText
+from email.mime.text import MIMEText        #email body
 import datetime #system date and time manipulation
