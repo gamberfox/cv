0a2710806d122e92f5e8156ad7154200b20fde3a
gamberfox <gamberfox@gmail.com>
2024-09-25 13:17:32 -0500

    testing h2 driver

--- kanji.py
+++
@@ -1,7 +0,0 @@
-from bs4 import BeautifulSoup as bs
-import requests
-
-
-response=requests.get(url)
-content=response.content#this is different from the global content in line 13
-soup=bs(content,'html.parser')
--- stock-api-service/src/main/java/com/emazon/stock_api_service/infrastructure/exception/BrandPersistenceException.java
+++
@@ -1,7 +0,0 @@
-package com.emazon.stock_api_service.infrastructure.exception;
-
-public class BrandPersistenceException extends RuntimeException {
-    public BrandPersistenceException(String message) {
-        super(message);
-    }
-}
